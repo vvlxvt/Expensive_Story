c40d82aa9c15644ecfd4421bca0311251e14f909
vvlxvt <vvlx@tutanota.com>
2024-01-08 10:24:10 +0400

    добавлен config

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ conf = config.load_config(None)
 TOKEN = conf.tg_bot.token
 
 WEB_SERVER_HOST = "127.0.0.1"
-WEB_SERVER_PORT = 8081
+WEB_SERVER_PORT = 80
 
 WEBHOOK_PATH = "/6006947703:AAFiIBqbYWhmZUl6l1crqb3ZbQI4CpiXkoU"
-BASE_WEBHOOK_URL = "https://1fee-94-43-154-7.ngrok-free.app"
+BASE_WEBHOOK_URL = "https://expensive-story-vvlxvt.amvera.io"
 
 async def on_startup(bot: Bot) -> None:
     await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",)
